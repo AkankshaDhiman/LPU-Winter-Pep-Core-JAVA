9d879ad53ff07dc56e7ff7ce6f5fa9902792d3d2
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-01-20 11:38:19 +0530

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,23 @@
+//○ Read a string from the console and:
+//■ Convert it to uppercase/lowercase.
+//■ Reverse the string.
+//■ Check if it is a palindrome
+import java.util.Scanner;
+public class Strings {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        String s=in.nextLine();
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        String rev="";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+        System.out.println("reversed string "+rev);
+    if(rev.equals(s)){
+        System.out.println("String is a Palindrome");
+    }else{
+        System.out.println("String is not a Plaindrome");
+    }
+    }
+}
