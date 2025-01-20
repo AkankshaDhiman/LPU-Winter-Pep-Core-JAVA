0a5c9c4e340eef32b5eda8e00ba3ab9a7f3e0d33
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-01-20 10:00:22 +0530

    abstraction

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//○ Create an abstract class Shape with an abstract method draw().
+//○ Extend it with Circle and Rectangle classes, implementing the draw()
+//method.
+//○ Test them in a main method.
+abstract class Shape {
+    abstract void draw();
+}
+
+class circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println(" Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println(" Rectangle ");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Shape circle = new circle();
+        Shape rectangle = new Rectangle();
+        circle.draw();
+        rectangle.draw();
     }
 }
