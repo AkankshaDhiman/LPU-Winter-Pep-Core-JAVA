9b5ae184114ea9e277a6abf1046eede429466804
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-02-11 14:44:31 +0530

    Hashing

---
+++ src/HashExample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HashExample {
+    public static void main(String[] args) {
+        String key="apple";
+        int hashCode = key.hashCode();
+        int index = Math.abs(hashCode) %10;
+        System.out.println("Hash Code : "+hashCode);
+        System.out.println("Index in Table : "+index);
+    }
+}
